6e9692a92f5608abf2b68eba07b038310386c44f
MFabri31 <48441333+MFabri31@users.noreply.github.com>
2020-07-21 09:30:50 -1200

    Update contraseña.py

--- contraseña.py
+++ contraseña.py
@@ -6,28 +6,26 @@ informar " cuenta bloqueada".
 """
 
 
-successMessage = ("ha iniciado sesión.")
-warnMessage = ("Las contraseñas no coinciden intente nuevamente.")
-errorMessage= ("Cuenta Bloqueada. intente más tarde")
-
-userName = input("Ingrese un nombre: ")
-userPass = input("Ingrese una contraseña: ")
-confirmPass = input("Confirme su contraseña: ")
+user_name = input("Ingrese un nombre: ")
+user_pass = input("Ingrese una contraseña: ")
+confirm_pass = input("Confirme su contraseña: ")
 
 count = 0
 
-if userPass == confirmPass:
-    print(userName, successMessage)
+if user_pass == confirm_pass:
+    print(f'{user_name}, ha iniciado sesión')
 
-elif userPass != confirmPass:
+elif user_pass != confirm_pass:
     while count < 5:
-        print(warnMessage)
-        confirmPass = input("Confirme su contraseña:")
-        if userPass == confirmPass:
-            print(userName,successMessage)
+        print('Las contraseñas no coinciden. intente nuevamente')
+        confirm_pass = input("Confirme su contraseña:")
+        if user_pass == confirm_pass:
+            print(f'{user_name} has iniciado sesión correctamente')
             break
         count = count + 1
-        print(errorMessage)
+        print('Cuenta bloqueada. Intente más tarde')
+
+    
 
     
 
