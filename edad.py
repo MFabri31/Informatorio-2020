2066de572b849b0b16dad98a7bd0de3b83628018
MFabri31 <48441333+MFabri31@users.noreply.github.com>
2020-07-21 09:48:54 -1200

    Update edad.py

--- edad.py
+++ edad.py
@@ -7,7 +7,7 @@ Solicitar la edad de una persona e informar si es mayor de edad o no
 
 person_age = int(input("Por favar ingrese la edad de una persona."))
 
-if personAge < 18:
+if person_age < 18:
     print('La persona es menor de edad')
 else:
     print('La persona es mayor de edad')
