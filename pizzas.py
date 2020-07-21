674d487944b86032a17c59a2f41ee2015b07123b
MFabri31 <48441333+MFabri31@users.noreply.github.com>
2020-07-21 09:40:16 -1200

    Update pizzas.py

--- pizzas.py
+++ pizzas.py
@@ -9,21 +9,18 @@ Escribir un programa que pregunte al usuario si quiere una pizza vegetariana o n
 
 """
 
-welcomeMessage = "Hola bienvenidos a pizzeria Bella Napoli. Tenemos pizzas vegeterianas y no vegetarianas para ofrecerte."
-warnMessage = "Ha ingresado valores incorrectos. intente nuevamente."
-menu = "A continuación elija la pizza que usted desea. a) Pizza vegetariana b) Pizza no vegetariana."
 
-print(welcomeMessage)
-menu = input(menu)
- 
+print('Hola bienvenidos a pizzeria Bella Napoli. Tenemos pizzas vegeterianas y no vegetarianas para ofrecerte.')
+menu = input('A continuación elija la pizza que usted desea.\na) Pizza vegetariana\nb) Pizza no vegetariana.\n>')
+
+
 if menu != "a" and menu != "b":
-    print(warnMessage)
-
+    print('Ha ingresado valores incorrectos. intente nuevamente.')
 
 if menu == "a":
-    ingredients = input("Por favor elija sus ingredientes. a) Pimiento  b) Rúcula.")
+    ingredients = input("Por favor elija sus ingredientes.\na) Pimiento\nb) Rúcula.")
     if ingredients != "a" and ingredients != "b":
-        print(warnMessage)
+        print('Ha ingresado valores incorrectos. intente nuevamente.')
     if ingredients == "a":
         print("Ha elejido una pizza vegetarina con los siguientes ingredientes: mozzarella, tomate y pimiento.")
     elif ingredients == "b":
@@ -32,7 +29,7 @@ if menu == "a":
 if menu == "b":
     ingredients = input("Por favor elija sus ingredientes. a) Jamón  b) Panceta.")
     if ingredients != "a" and ingredients != "b":
-        print(warnMessage)
+        print('Ha ingresado valores incorrectos. intente nuevamente.')
     if ingredients == "a":
         print("Ha elejido una pizza no vegetarina con los siguientes ingredientes: mozzarella, tomate y jamón.")
     elif ingredients == "b":
