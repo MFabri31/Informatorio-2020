1591d652db525e7950b9f17949787f7852cf328a
MFabri31 <48441333+MFabri31@users.noreply.github.com>
2020-07-22 00:35:25 -1200

    Update biologos.py

--- desafíos/listas-tuplas-diccionarios/biologos.py
+++ desafíos/listas-tuplas-diccionarios/biologos.py
@@ -3,30 +3,22 @@ Crea un diccionario donde la clave sea el nombre de biólogos y el valor sea el 
 Tendrás que ir pidiendo contactos hasta el usuario diga que no quiere insertar mas. No se podrán insertar nombres repetidos.
 
 """
-
 biologists = {}
 
-flag = True
 
-while flag:
-    addContact = input('Agregar contacto [S/N]: ')
+while True:
+    add_contact = input('Agregar contacto [S/N]: ')
     
-    if addContact == 'n':
+    if add_contact == 'n':
         print('Cerrando...')
         flag = False
-    elif addContact == 's':
+    elif add_contact == 's':
         name = input('Nombre: ').capitalize()
         email = input('Email: ')
         if name not in biologists:
             biologists[name] = email
         else:
             print('Este nombre ya se encuentra agendado.')
-          
-    
+
 
 print(f'Contactos: {biologists}')
-
-
-
-
-
