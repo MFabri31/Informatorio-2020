1d18412234e29f2a39c5c89bfb4ac6bcb70098e1
MFabri31 <48441333+MFabri31@users.noreply.github.com>
2020-07-22 00:30:41 -1200

    Update arboles_plantados.py

--- desafíos/funciones/arboles_plantados.py
+++ desafíos/funciones/arboles_plantados.py
@@ -3,49 +3,49 @@ Realiza una función separar(lista) que tome una lista que tenga datos de cantid
 Qué cantidad de ciudades han plantado más de 100 árboles durante cuarentena.
 """
 
-totalTrees = []
+total_trees = []
+
 
 def getData():
     while True:
         try:
-            totalCities = int(input('Indique el total de ciudades que han plantados árboles: \n>'))
+            total_cities = int(input('Indique el total de ciudades que han plantados árboles: \n>'))
         except ValueError:
             print('Error! debes ingresar un valor numérico. Intente nuevamente.')
         else:
             break
 
-    for value in range(0,totalCities):
+    for value in range(0,total_cities):
          value += 1
          trees = int(input(f'Árboles plantados en la ciudad {value}: \n> '))
-         totalTrees.append(trees)
+         total_trees.append(trees)
+
 
 getData()
 
 
-def divide(totalTrees):
+def divide(total_trees):
     list1 = []
     list2 = []
 
     city = 0
 
-    for value in totalTrees:
+    for value in total_trees:
         if value > 100:
            list1.append(value)
            city += 1
         elif value < 100:
             list2.append(value)
-    
+            
     return list1,list2,city
 
 
-list1,list2,city = divide(totalTrees)  
+list1,list2,city = divide(total_trees)  
+
 
 print('------------')
+
 
 print(f'Lista 1 con mas de 100 árboles: {list1}')
 print(f'Lista 2 con el resto de árboles: {list2}')
 print(f'Total de ciudades que han plantado mas de 100 árboles: {city}')
-
-  
-
-
