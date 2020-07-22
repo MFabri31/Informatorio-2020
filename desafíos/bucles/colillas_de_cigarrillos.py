c00abdc3d63fd894257706a61e74c10b41845247
MFabri31 <48441333+MFabri31@users.noreply.github.com>
2020-07-22 00:18:14 -1200

    Update colillas_de_cigarrillos.py

--- desafíos/bucles/colillas_de_cigarrillos.py
+++ desafíos/bucles/colillas_de_cigarrillos.py
@@ -5,49 +5,49 @@ Realizá un programa que permita registrar cantidad de colillas recolectadas por
 
 """
 
-people100 = 0
-people200 = 0
-people300 = 0
+people_100 = 0
+people_200 = 0
+people_300 = 0
 
 
 while True:
     try:
-        totalPeople = int(input('Ingrese el total de personas que han recolectado colillas: \n> '))
+        total_people = int(input('Ingrese el total de personas que han recolectado colillas: \n> '))
     except ValueError:
         print('Error! Solo se permite el ingreso valores numéricos.')
         print('')
     else:
         break
-
-
-for value in range(0,totalPeople):
+        
+        
+for value in range(0,total_people):
     value += 1
     
     while True:
         try:
-            cigaretteButts = int(input(f'Persona n°{value} colillas recolectadas:\n> '))
+            cigarette_butts = int(input(f'Persona n°{value} colillas recolectadas:\n> '))
         except ValueError:
             print('Debe ingresar un valor numérico.')
             print('')
         else:
             break
-
-    
-    if cigaretteButts < 100:
-        people100 = people100 + 1
+            
+            
+    if cigarette_butts < 100:
+        people_100 = people_100 + 1
         
-    elif cigaretteButts < 200:
-        people200 = people200 + 1
+    elif cigarette_butts < 200:
+        people_200 = people_200 + 1
     else:
-        people300 = people300 + 1
-
-    percentage100 = round((people100 / totalPeople) * 100)
-    percentage200 = round((people200 / totalPeople) * 100)
-    percentage300 = round((people300 / totalPeople) * 100)  
-
-
-
-print(f'Total de personas: {totalPeople}')
-print(f'Menos de 100 colillas: {people100} / Porcentaje {percentage100}%')
-print(f'Mas de 100 y menos de 200 colillas: {people200} / Porcentaje {percentage200}%')
-print(f'Mas de 300 colillas: {people300} / Porcentaje {percentage300}%')
+        people_300 = people_300 + 1
+        
+        
+    percentage_100 = round((people_100 / total_people) * 100)
+    percentage_200 = round((people_200 / total_people) * 100)
+    percentage_300 = round((people_300 / total_people) * 100)  
+    
+    
+print(f'Total de personas: {total_people}')
+print(f'Menos de 100 colillas: {people_100} / Porcentaje {percentage_100}%')
+print(f'Mas de 100 y menos de 200 colillas: {people_200} / Porcentaje {percentage_200}%')
+print(f'Mas de 300 colillas: {people_300} / Porcentaje {percentage_300}%')
