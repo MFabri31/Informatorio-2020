f2ca953ffac74d296cc7f183efa35fdf94b168c4
MFabri31 <48441333+MFabri31@users.noreply.github.com>
2020-07-22 00:38:02 -1200

    Update contaminacion.py

--- desafíos/listas-tuplas-diccionarios/contaminacion.py
+++ desafíos/listas-tuplas-diccionarios/contaminacion.py
@@ -2,20 +2,17 @@
 1.Escribir un programa que pregunte a diferentes personas cuánto conocen sobre contaminación del 1 al 10, almacene estos valores en una lista y los muestre por pantalla ordenados de menor a mayor. 
 
 """
-
 from random import randint
-
-message = 'Indique del 1 al 10 cuanto conoce sobre la contaminación'
-
-totalPeople = randint(2,7)
 
 numbers = []
 
-for value in range(0,totalPeople):
-    num = int(input(f'{message}: '))
+total_people = randint(2,7)
+
+for value in range(0,total_people):
+    num = int(input('Indique del 1 al 10 cuanto conoce sobre la contaminación: '))
     numbers.append(num)
+
 
 numbers.sort()
 
 print(f"Lista ordenada: {numbers}")
-    
